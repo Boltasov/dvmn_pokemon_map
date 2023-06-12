fbf781c631a76a2c7c55dc09574fb6e543882784
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-06-12 06:57:41 +0300

    fix potential bug with defining time of pokemon appearance

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -28,7 +28,8 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    pokemons_entities = PokemonEntity.objects.filter(appeared_at__lte=localtime(), disappeared_at__gte=localtime())
+    time = localtime()
+    pokemons_entities = PokemonEntity.objects.filter(appeared_at__lte=time, disappeared_at__gte=time)
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
 
